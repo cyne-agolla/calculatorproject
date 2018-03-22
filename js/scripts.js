a0b56b077fb87a007b3d74d7bbbd09d537f41ca7
Cynthia Agollah <cyneagollah@gmail.com>
2018-03-22 09:47:36 +0300

    add changes

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+      var saySomething = function(whatToSay) {
+        alert(whatToSay);
+      };
+
+      var add = function(number1, number2) {
+        return number1 + number2;
+      };
+
+      var result = add(3, 5);
+      var outputText = "The sum is " + result + ".";
+      saySomething(outputText);
